423d2e2c3c4a4d46f7b8f7eaa40f0f93435d3ea8
Modesto Echezarreta <echezarreta.m@gmail.com>
2025-02-11 23:32:12 -0600

    added diagnostic support

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ A Language Server built for the education purpose of understanding **WHAT** LSP 
 
 It doesn't do anything specual for any particular language. It is focused on helping understand what tools like LSP **do**.
 
-This LSP was tested with Neovim, but would likely work inside VS C*de.
+This LSP was tested with Neovim, but would likely work inside VS Code.
--- main.go
+++ main.go
@@ -55,7 +55,17 @@ func handleMessage(logger *log.Logger, writer io.Writer, state analysis.State, m
 			return
 		}
 		logger.Printf("Opened to: %s", request.Params.TextDocument.URI)
-		state.OpenDocument(request.Params.TextDocument.URI, request.Params.TextDocument.Text)
+		diagnostics := state.OpenDocument(request.Params.TextDocument.URI, request.Params.TextDocument.Text)
+		writeResponse(writer, lsp.PublishDiagnosticsNotification{
+			Notification: lsp.Notification{
+				RPC:    "2.0",
+				Method: "textDocument/publishDiagnostics",
+			},
+			Params: lsp.PublishDiagnosticsParams{
+				URI:         request.Params.TextDocument.URI,
+				Diagnostics: diagnostics,
+			},
+		})
 
 	case "textDocument/didChange":
 		var request lsp.TextDocumentDidChangeNotification
@@ -65,7 +75,17 @@ func handleMessage(logger *log.Logger, writer io.Writer, state analysis.State, m
 		}
 		logger.Printf("Changed: %s", request.Params.TextDocument.URI)
 		for _, change := range request.Params.ContentChanges {
-			state.OpenDocument(request.Params.TextDocument.URI, change.Text)
+			diagnostics := state.OpenDocument(request.Params.TextDocument.URI, change.Text)
+			writeResponse(writer, lsp.PublishDiagnosticsNotification{
+				Notification: lsp.Notification{
+					RPC:    "2.0",
+					Method: "textDocument/publishDiagnostics",
+				},
+				Params: lsp.PublishDiagnosticsParams{
+					URI:         request.Params.TextDocument.URI,
+					Diagnostics: diagnostics,
+				},
+			})
 		}
 
 	case "textDocument/hover":
